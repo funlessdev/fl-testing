ea5cc4ae08f270bceecb8df305a2567d4b052107
giusdp <dpl.gsp@tutanota.com>
2022-09-02 17:00:26 +0200

    feat(vagrant): add bootstrap to install docker and fl-cli

--- vagrant/bootstrap_core.sh
+++ vagrant/bootstrap_core.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 
-echo "[TASK 1] Get Core release"
-
-echo "[TASK 2] Start Core release"
-
-
--- vagrant/bootstrap_worker.sh
+++ vagrant/bootstrap_worker.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 
-echo "[TASK 1] Get Worker release"
-
-echo "[TASK 2] Start Worker release"
